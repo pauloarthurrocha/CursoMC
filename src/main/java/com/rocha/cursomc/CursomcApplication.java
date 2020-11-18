46026eb4ed78d64c4783b2297d3f2ae2466d0fff
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-18 09:36:03 -0300

    Criando operaçao de iniciaçao

--- src/main/java/com/rocha/cursomc/CursomcApplication.java
+++ src/main/java/com/rocha/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.rocha.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rocha.cursomc.domain.Categoria;
+import com.rocha.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
