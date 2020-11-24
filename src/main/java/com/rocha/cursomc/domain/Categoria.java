7e061eaa803eb8843d82a0956b5aea177b869108
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-24 15:28:01 -0300

    Validaçao sintatica com Bean Validation

--- src/main/java/com/rocha/cursomc/domain/Categoria.java
+++ src/main/java/com/rocha/cursomc/domain/Categoria.java
@@ -9,7 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -21,6 +23,9 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min = 5, max = 80, message = "O tamanaho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	@ManyToMany(mappedBy = "categorias")
--- src/main/java/com/rocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/rocha/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rocha.cursomc.domain.Categoria;
+import com.rocha.cursomc.dto.CategoriaDTO;
 import com.rocha.cursomc.repositories.CategoriaRepository;
 import com.rocha.cursomc.services.exceptions.DataIntegrityException;
 import com.rocha.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
