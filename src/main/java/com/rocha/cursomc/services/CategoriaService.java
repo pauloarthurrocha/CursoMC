fa0f088211f18e7296cfaa88d37cff4d4f21b68a
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-27 12:46:04 -0300

    Inserindo novo cliente com POST

--- src/main/java/com/rocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/rocha/cursomc/services/CategoriaService.java
@@ -37,8 +37,9 @@ public class CategoriaService {
 
 	// quando o id nao e null ele atualiza por isso o PUT
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -62,6 +63,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
