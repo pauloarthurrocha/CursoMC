a6170ab721145dae3b5cb2e9481713ba7541d11e
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-12-03 12:21:30 -0300

    Inserindo Pedido

--- src/main/java/com/rocha/cursomc/domain/ItemPedido.java
+++ src/main/java/com/rocha/cursomc/domain/ItemPedido.java
@@ -10,18 +10,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
 
-	public ItemPedido()	{
-		
+	public ItemPedido() {
+
 	}
+
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
@@ -34,40 +35,56 @@ public class ItemPedido implements Serializable {
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
+
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
+
 	public Double getDesconto() {
 		return desconto;
 	}
+
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
+
 	public Integer getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
+
 	public Double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -75,6 +92,7 @@ public class ItemPedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -91,6 +109,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
 
 }
